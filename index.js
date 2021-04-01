d99c9b86a001819bd3f0dfb26fdfce258021138b
Dequisa <Dequisas@gmail.com>
2021-04-01 19:31:24 -0400

    just about done

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-// let button = document.querySelector("button");
-// button.addEventListener("click",drawCard);
+let ul = document.getElementById("cardList");
+let p = document.getElementById("remaining");
 console.log('good luck!')
+var deck_Id=""
+
+
 
 async function shuffleDeck(){
   try{
-  let shuffle= await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-  deck_Id= shuffle.data.deck_id
-  let hold =drawCard(deck_Id)
+   let shuffle= await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+   deck_Id=   shuffle.data.deck_id
+   console.log(shuffle.data)
+  // let hold =drawCard(deck_Id)
     }catch(err){console.log("This is the error:", err)}
 }
 
 async function drawCard(id){
   try{
-    let button = document.querySelector("button");
-button.addEventListener("click",drawCard);
     let select = document.getElementById('select_value');
     let numberOfCards = select.options[select.selectedIndex].text;
     console.log(numberOfCards)
-    let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=${numberOfCards}`)
-    let cards = draw.data.cards
+    let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_Id}/draw/?count=${numberOfCards}`)
+    let cards = await draw.data.cards
     for(const element of cards){
+      // console.log(element)
       let img = document.createElement("img");
-      let ul = document.getElementById("cardList")
       let li = document.createElement("li")
       li.id = element.value;
       img.src = element.image;
      li.appendChild(img)
      ul.appendChild(li);
-
     }
-
-
+    ul.replaceChild(ul);
+    p.textContent = `${draw.data.remaining} card(s) left`
   }catch(err){console.log("this is your error:",err)}
+
+  
 }
 
-// shuffleDeck()
+shuffleDeck()
+
+let button = document.querySelector("button");
+button.addEventListener("click",drawCard);
